73f6efa1c93b4d1c40690546d4428286ff22c7a7
Kien-Tran <ttkien2004@gmail.com>
2024-11-23 17:07:43 +0700

    Fix backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,28 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
+const hospitalRoutes = require("./routes/hospitalRoutes");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.use(cors());
+
+app.use("/api/hospital", hospitalRoutes);
+
+const db = mysql.createConnection({
+  host: "",
+  user: "root",
+  password: "kien2004",
+  database: "",
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listen to port ${process.env.PORT}`);
+});
